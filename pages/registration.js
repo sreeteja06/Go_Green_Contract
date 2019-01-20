038c822f39abbeca582a105e76acadc61c409596
sreeteja06 <sreeteja.muthyala@gmail.com>
2019-01-20 19:25:44 +0530

    before removing next js

--- pages/registration.js
+++ pages/registration.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-var ReactDOMServer = require('react-dom/server');
-var HtmlToReactParser = require('html-to-react').Parser;
-import './registration.css';
- 
-var htmlInput = '<div><h1>Title</h1><p>A paragraph</p></div>';
-var htmlToReactParser = new HtmlToReactParser();
-var reactElement = htmlToReactParser.parse(htmlInput);
-// var reactHtml = ReactDOMServer.renderToStaticMarkup(reactElement);
-
+import React, { Component } from "react";
 class registration extends Component {
-    state = {  }
-    render() { 
-        return ( reactElement );
-    }
+  state = {};
+  render() {
+    return (
+      <div>
+        <Head>
+          <title>My styled page</title>
+          <link href="/statics/registration.css" rel="stylesheet" />
+        </Head>
+        <h1>Hello</h1>
+      </div>
+    );
+  }
 }
- 
+
 export default registration;
